f3cb2e26e8e8f0b2a9b96f9c912b3c980cda9925
asmaismail <asmaismail202020@gmail.com>
2022-11-03 12:43:29 +0500

    upload

---
+++ JS-CSOFT/contact us.php
@@ -0,0 +1,235 @@
+<?php
+include_once 'navbar.php';
+?>
+<body>
+   
+    <main>
+    <!--location-->
+    <div class="container">
+        <div class="row text-center d-flex justify-content-center align-items-center">
+          <div class="col-md-3 mt-5">
+            <svg xmlns="http://www.w3.org/2000/svg" style="color:rgb(9, 9, 100)" width="50" height="50" fill="currentColor"
+              class="bi bi-geo-alt" viewBox="0 0 16 16">
+              <path
+                d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
+              <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+            </svg>
+            <p class="mt-3" style="color:rgb(14, 106, 129);"><b>Head Office</b></p>
+            <p class="text-center">P-165 B, First & Second Floor, Millat Chowk, Faisalabad, Punjab, Pakistan - 38000</p>
+          </div>
+          <div class="col-md-3 mt-5">
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" style="color:rgb(9, 9, 100)" fill="currentColor"
+              class="bi bi-geo-alt" viewBox="0 0 16 16">
+              <path
+                d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
+              <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+            </svg>
+            <p class="mt-3" style="color:rgb(14, 106, 129);"><b>UAE Office</b></p>
+            <p class="text-center">Al-Manar, Hajj Umrah Building, Behind E Max, Hamdan Street, Abu Dhabi, UAE.</p>
+          </div>
+        </div>
+        <div class="row text-center d-flex justify-content-center align-items-center">
+          <div class="col-md-3 mt-5">
+            <svg xmlns="http://www.w3.org/2000/svg" style="color:rgb(9, 9, 100)" width="50" height="50" fill="currentColor"
+              class="bi bi-geo-alt" viewBox="0 0 16 16">
+              <path
+                d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
+              <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+            </svg>
+            <p class="mt-3" style="color:rgb(14, 106, 129);"><b>Liasion Office 1</b></p>
+            <p class="text-center">P-165 B, First & Second Floor, Millat Chowk, Faisalabad, Punjab, Pakistan - 38000</p>
+          </div>
+          <div class="col-md-3 mt-5">
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" style="color:rgb(9, 9, 100)" fill="currentColor"
+              class="bi bi-geo-alt" viewBox="0 0 16 16">
+              <path
+                d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
+              <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+            </svg>
+            <p class="mt-3" style="color:rgb(14, 106, 129);"><b>Liasion Office 2</b></p>
+            <p class="text-center">Al-Manar, Hajj Umrah Building, Behind E Max, Hamdan Street, Abu Dhabi, UAE.</p>
+          </div>
+          <div class="col-md-3 mt-5">
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" style="color:rgb(9, 9, 100)" fill="currentColor"
+              class="bi bi-geo-alt" viewBox="0 0 16 16">
+              <path
+                d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
+              <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+            </svg>
+            <p class="mt-3" style="color:rgb(14, 106, 129);"><b>Liasion Office 3 </b></p>
+            <p class="text-center">Al-Manar, Hajj Umrah Building, Behind E Max, Hamdan Street, Abu Dhabi, UAE.</p>
+          </div>
+        </div>
+        <div class="row text-center d-flex justify-content-center align-items-center">
+          <div class="col-md-3 mt-5">
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" style="color:rgb(9, 9, 100)" fill="currentColor"
+              class="bi bi-headset" viewBox="0 0 16 16">
+              <path
+                d="M8 1a5 5 0 0 0-5 5v1h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V6a6 6 0 1 1 12 0v6a2.5 2.5 0 0 1-2.5 2.5H9.366a1 1 0 0 1-.866.5h-1a1 1 0 1 1 0-2h1a1 1 0 0 1 .866.5H11.5A1.5 1.5 0 0 0 13 12h-1a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1h1V6a5 5 0 0 0-5-5z" />
+            </svg>
+            <p class="mt-3"><b style="color:rgb(14, 106, 129)" ;>Phone:</b> +92 41 85800</p>
+            <p class="mt-3"><b style="color:rgb(14, 106, 129)" ;>Mobile:</b> +92 314 5000083</p>
+          </div>
+          <div class="col-md-3 mt-5">
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" style="color:rgb(9, 9, 100)" fill="currentColor"
+              class="bi bi-envelope" viewBox="0 0 16 16">
+              <path
+                d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
+            </svg>
+            <p class="mt-3"><b style="color:rgb(14, 106, 129)" ;>Email:</b> info@csoftsystems.com</p>
+            <p class="mt-3"><b style="color:rgb(14, 106, 129)" ;>Web:</b> www.csoftsystems.com</p>
+    
+          </div>
+          <div class="col-md-3 mt-5">
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" style="color:rgb(9, 9, 100)" fill="currentColor"
+              class="bi bi-clock" viewBox="0 0 16 16">
+              <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
+              <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
+            </svg>
+            <p class="mt-3"><b style="color:rgb(14, 106, 129)" ;>Week Days:</b> 9:00am - 9:00pm</p>
+            <p class="mt-3"><b style="color:rgb(14, 106, 129)" ;>Sunday:</b> Closed</p>
+          </div>
+        </div>
+      </div>
+      <!--location ends-->
+      <!--form starts-->
+    <h2 class="text-center mt-5"><b><i style="color:rgb(49, 49, 218)">Do you want to</i> <i style="color:rgb(62, 154, 185)">Contact us</i></b></h2>
+      <div class="imgmain">
+
+        <div class="img">
+            
+        </div>
+        <div class="text">
+            <form>
+                <div class="container d-flex">
+                    <div class="col-md-1"></div>
+                    <div class="col-md-5">
+                        <br><br><br><br><br><br><br><br>
+                        <h2 class="text-center mt-5"><b><i style="color:rgb(49, 49, 218)">Request a Call back</i></b></h2>
+                      <p>If you are interested to know more about our plans, our team members are keen to speak with you about the question you have in your mind. Fill the request a call back form and our team members will call you as soon as possible.
+                    </p>
+                    <h5 class="text-center mt-5"><i style="color:rgb(49, 49, 218)">+92 305 50000</i></h5>
+                    </div>
+                    <div class="col-md-6"><br>
+                        <h2 class="text-center mt-5"><i style="color:rgb(49, 49, 218)">Drop your message</i></h2>
+                        <form class="mt-5">
+                            <div class="row d-flex mt-5">
+                                <div class="col-md-6">
+                                    <br>
+                                    <input type="text" placeholder="First Name" style="width:100% ;"></div>
+                                    <div class="col-md-6"><br>
+                                    <input type="text" placeholder="Last Name" style="width:100% ;"></div>
+                            </div>
+                            <div class="row mt-3">
+                            </div>
+                            <div class="row d-flex ">
+                                <div class="col-md-6">
+                                    <input type="text" placeholder="Phone" style="width:100% ;"></div>
+                                    <div class="col-md-6">
+                                    <input type="text" placeholder="Email" style="width:100% ;"></div>
+                            </div>
+                            <textarea class="form-control col-md-12 mt-3" rows="5" id="comment">Message</textarea>
+                            <button class="btn mt-3"><i class="fa-solid fa-paper-plane"></i> Submitt</button>
+                        </form>
+
+                    </div>
+                </div>
+            </form>
+        </div>
+      </div>
+      <!--form ends-->
+      <!--map starts-->
+      <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+      <div class="container-fluid mt-5">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27234.745319309608!2d73.07556754984884!3d31.432218327748448!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x392269b7bed2188b%3A0x80f0a61eeb47b5bf!2sCSOFT%20Systems!5e0!3m2!1sen!2s!4v1654512000150!5m2!1sen!2s"
+          width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+      <!--map ends-->
+      <!--footer-->
+      <footer>
+        <div class="container-fluid">
+            <div class="footer">
+              <div class="row d-flex">
+                <div class="col-md-4  col-sm-3 col-xsm-3 mt-5">
+                  <img src="Images/csoft footer-1.png" height="50vh" class="ms-5 text-justify-center">
+                  <p class="mt-4 m-3 ms-5">Get great business solution with CSOFT Systems with our unique hosting plans and
+                    support for growing your
+                    business.</p>
+                  <div class="input-group  col-md-2">
+                    <input type="text" class="m-3 ms-5" style="height:5vh ; background:none; color:white"
+                      placeholder="Message us here..." aria-label="Recipient's username" aria-describedby="basic-addon2">
+                    <span class="input-group-text imp" id="basic-addon2"><svg xmlns="http://www.w3.org/2000/svg" width="16"
+                        height="16" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
+                        <path
+                          d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
+                      </svg></span>
+                  </div>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" fill="currentColor"
+                    class="bi bi-facebook mt-3 ms-5" viewBox="0 0 16 16">
+                    <path
+                      d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
+                  </svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" fill="currentColor"
+                    class="bi bi-instagram m-1 mt-4" viewBox="0 0 16 16">
+                    <path
+                      d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z" />
+                  </svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" fill="currentColor"
+                    class="bi bi-pinterest m-1 mt-4" viewBox="0 0 16 16">
+                    <path
+                      d="M8 0a8 8 0 0 0-2.915 15.452c-.07-.633-.134-1.606.027-2.297.146-.625.938-3.977.938-3.977s-.239-.479-.239-1.187c0-1.113.645-1.943 1.448-1.943.682 0 1.012.512 1.012 1.127 0 .686-.437 1.712-.663 2.663-.188.796.4 1.446 1.185 1.446 1.422 0 2.515-1.5 2.515-3.664 0-1.915-1.377-3.254-3.342-3.254-2.276 0-3.612 1.707-3.612 3.471 0 .688.265 1.425.595 1.826a.24.24 0 0 1 .056.23c-.061.252-.196.796-.222.907-.035.146-.116.177-.268.107-1-.465-1.624-1.926-1.624-3.1 0-2.523 1.834-4.84 5.286-4.84 2.775 0 4.932 1.977 4.932 4.62 0 2.757-1.739 4.976-4.151 4.976-.811 0-1.573-.421-1.834-.919l-.498 1.902c-.181.695-.669 1.566-.995 2.097A8 8 0 1 0 8 0z" />
+                  </svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" fill="currentColor"
+                    class="bi bi-linkedin m-1 mt-4" viewBox="0 0 16 16">
+                    <path
+                      d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
+                  </svg>
+                </div>
+                <div class="col-md-2 col-sm-2 col-xsm-2 mt-5">
+                  <h3 class="text-primary">Useful Links</h3>
+                  <p><a href="#">About us</p>
+                  <p><a href="#">Contact us</a></p>
+                  <p><a href="#">Services</a></p>
+                  <p><a href="#">Blog</a></p>
+                  <p><a href="#">Carrers</a></p>
+                  <p><a href="#">FAQ's</a></p>
+                </div>
+                <div class="col-md-3 col-sm-3 col-xsm-3 mt-5">
+                  <h3 class="text-primary">Popular Tags</h3>
+                  <button type="button" class="btn btn-outline-primary m-2">Andriod Apps</button><br>
+                  <button type="button" class="btn btn-outline-primary m-2">UI Design</button>
+                  <button type="button" class="btn btn-outline-primary m-2">SEO</button><br>
+                  <button type="button" class="btn btn-outline-primary m-2">Graphic DEsign</button><br>
+                  <button type="button" class="btn btn-outline-primary m-2">Services</button>
+                  <button type="button" class="btn btn-outline-primary m-2">CCTV</button> <br>
+                  <button type="button" class="btn btn-outline-primary m-2">Networking</button>
+                </div>
+                <div class="col-md-2  col-sm-3 col-xsm-3 mt-5">
+                  <h3 class="text-primary">Conatct Us</h3>
+                  <p>CSOFT Systems P-165 B, First & Second Floor, Millat Chowk, Faisalabad, Punjab, Pakistan - 38000</p>
+                  <p>Mobile : +92 314 5000083</p>
+                  <p>E-mail : info@csoftsystems.com</p>
+                </div>
+              </div>
+              <!--row-->
+              <div class="row">
+                <hr>
+                <p class="text-center">Copyright ©2020 CSOFT. All Rights Reserved</p>
+              </div>
+            </div>
+            <!--footer-->
+          </div>
+        
+
+      </footer>
+
+    </main>
+    <script src="bootstrap-5.1.3-dist/js/bootstrap.min.js"></script>
+        <script src="aos-master/dist/aos.js"></script> 
+        <script>
+            AOS.init();
+          </script>
+</body>
+</html>
